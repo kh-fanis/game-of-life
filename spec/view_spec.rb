4421075b82116156253ba97c8b278eff21597591
kh-fanis <kh-fanis@rambler.ru>
2016-01-31 00:25:36 +0300

    one another test added

--- spec/view_spec.rb
+++ spec/view_spec.rb
@@ -6,4 +6,8 @@ describe View do
   it 'should open view file' do
     expect(view.template).to eql "hello world. Now <%= Time.now.hour %>:<%= Time.now.min %>\n"
   end
+
+  it 'should correctly parse code inside <%= code %>' do
+    expect(view.parse).to eql "hello world. Now #{ Time.now.hour }:#{Time.now.min}"
+  end
 end
