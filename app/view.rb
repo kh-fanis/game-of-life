2238fb9bd7925acf56babb921e2fbc02acd0eede
kh-fanis <kh-fanis@rambler.ru>
2016-01-31 12:38:19 +0300

    one more test added

--- app/view.rb
+++ app/view.rb
@@ -4,9 +4,19 @@ class View
   def initialize action_file
     @action = action_file
     File.open("#{File.dirname(__FILE__)}/views/#{@action}.view", 'r') { |file| puts @template = file.read }
+    parse
   end
 
   def parse
-    template.gsub(/<%=\s*[^<]+\s*%>/){ |code| eval /<%=\s*(.*)\s*%>/.match(code)[1]}
+    @parsed_text = template.gsub(/<%=\s*[^<]+\s*%>/){ |code| eval /<%=\s*(.*)\s*%>/.match(code)[1]}
+  end
+
+  def render
+    clear_screen
+    print @parsed_text.gsub(/<h5>[^<]+<\/h5>/){ |code| eval /<h5>(.*)<\/h5>/.match(code)[1]}
+  end
+
+  def clear_screen
+    system 'clear'
   end
 end
--- spec/view_spec.rb
+++ spec/view_spec.rb
@@ -2,6 +2,7 @@ require_relative 'spec_helper.rb'
 
 describe View do
   let(:view) { View.new :view_test_file }
+  let(:view_with_data) { View.new :for_test_with_data }
 
   it 'should open view file' do
     expect(view.template).to eql "hello world. Now <%= Time.now.hour %>:<%= Time.now.min %>\n"
@@ -12,6 +13,12 @@ describe View do
   end
 
   it 'should render text out to screen' do
-    expect { view.render }.to output('hello world. Now #{ Time.now.hour }:#{ Time.now.min }\n').to_stdout
+    view
+    expect { view.render }.to output("hello world. Now #{ Time.now.hour }:#{ Time.now.min }\n").to_stdout
+  end
+
+  it 'should render text with data' do
+    view_with_data
+    expect { view_with_data.render some_variable: 'this is from variable', another_one_variable: 'this from variable too' }.to output('This should correctly output the data\nthis is from variable\nthis from variable too\n')
   end
 end
